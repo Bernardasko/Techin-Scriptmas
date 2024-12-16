c98efbec5d6ad5b1ac0f73ac51457b03cdbb8b03
Bernardasko <115994039+Bernardasko@users.noreply.github.com>
2024-12-16 10:37:47 +0200

    DAY-16

---
+++ 16-DAY/santa.js
@@ -0,0 +1,44 @@
+// Morse code mapping for each letter
+const alphaMorse = {
+  A: ".-", 
+  B: "-...", 
+  C: "-.-.", 
+  D: "-..", 
+  E: ".", 
+  F: "..-.", 
+  G: "--.", 
+  H: "....", 
+  I: "..", 
+  J: ".---", 
+  K: "-.-", 
+  L: ".-..",
+  M: "--", 
+  N: "-.", 
+  O: "---", 
+  P: ".--.", 
+  Q: "--.-", 
+  R: ".-.", 
+  S: "...", 
+  T: "-", 
+  U: "..-", 
+  V: "...-", 
+  W: ".--", 
+  X: "-..-", 
+  Y: "-.--",
+  Z: "--..", 
+};
+
+function elfMorse(message) {
+  return message
+    .toUpperCase()
+    .split("")
+    .map((char) => {
+      const morseCode = alphaMorse[char];
+      if (!morseCode) return "";
+      return morseCode.replace(/\./g, "ho ").replace(/-/g, "hoho ");
+    })
+    .join(" ");
+}
+
+console.log(elfMorse("MERRY"));
+
